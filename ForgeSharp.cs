c633b598f76c6a2034b7954b898effcc24db23d2
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2025-05-16 12:30:20 -1000

    Add files via upload

--- ForgeSharp.cs
+++ ForgeSharp.cs
@@ -2,18 +2,23 @@ using System;
 using System.Xml.Linq;
 
 namespace XML_Studio
-{ class Program 
-    { static void Main(string[] args)
-        {Document xDoc = new XDocument(
-            new XDeclaration("1.0", "utf-8", "yes"),
-            new XElement("RootName",
-                new XElement("Child1","data1"),
-                new XElement("Child2",
-                    new XAttribute("Name", "CodeJunk"), "myvalue")
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDoc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("RootName",
+                    new XElement("Child1", "data1"),
+                    new XElement("Child2",
+                        new XAttribute("Name", "CodeJunk"), "myvalue")
                 )
             );
 
-            xDoc.Save("c:\\users\\defaultuser0\\Documents\\index.xml");
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                          @"\source\repos\Xbuilder\index.xml";
+            xDoc.Save(path);
         }
     }
 }
